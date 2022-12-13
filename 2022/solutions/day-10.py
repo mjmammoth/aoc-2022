aa88af11b0593861631e29f9308df8ba5b8f6447
mjmammoth <awsumtoast@gmail.com>
2022-12-13 06:07:21 +0000

    Day 10 Part 2

--- 2022/solutions/day-10.py
+++ 2022/solutions/day-10.py
@@ -1,23 +1,40 @@
 from aoc import read_input
+import math
 
-class Register:
+class Register():
     def __init__(self):
         self.value = 1
         self.cycles = 0
         self.history = []
-    
-    def cycle(self):
-        self.cycles += 1
-        self.history.append(self.value)
+        self.screen = [[] for _ in range(6)]
 
     def addx(self, value):
         self.cycle()
-        self.value += value
         self.cycle()
+        self.value += value
+    
+    def cycle(self):
+        if self.cycles == 38:
+            print(1)
+        self.draw()
+        self.history.append(self.value)
+        self.cycles += 1
+    
+    def draw(self):
+        self.row = math.floor(self.cycles/40)
+        if self.cycles - (self.row * 40) in range(self.value-1, self.value+2):
+            self.screen[self.row] += '#'
+        else:
+            self.screen[self.row] += '.'
+
+    def render(self):
+        for row in self.screen:
+            print(''.join(row))
 
 register = Register()
-
 aocInput = read_input(__file__)
+signalStrengthPart1 = 0
+
 for instruction in aocInput:
     if instruction == 'noop':
         register.cycle()
@@ -25,8 +42,10 @@ for instruction in aocInput:
         _, value = instruction.split()
         register.addx(int(value))
 
-signalStrengthPart1 = 0
-for i in range(18,220,40):
+# Part 1
+for i in range(19,240,40):
     signalStrengthPart1 += register.history[i] * (i+2)
-
 print(f"Part 1: {signalStrengthPart1}")
+
+# Part 2
+register.render()
